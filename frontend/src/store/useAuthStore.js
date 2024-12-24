94e2295c8d383f4d2294988bce74e5f8aaba76a0
Harshit <sainiharshit322@gmail.com>
2024-12-24 21:43:52 +0530

    added some of the functions

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -13,7 +13,7 @@ export const useAuthStore = create((set) => ({
         const res = await axiosInstance.get("/auth/check");
   
         set({ authUser: res.data });
-        get().connectSocket();
+        // get().connectSocket();
       } catch (error) {
         console.log("Error in checkAuth:", error);
         set({ authUser: null });
@@ -23,6 +23,56 @@ export const useAuthStore = create((set) => ({
     },
 
     signup: async(data) => {
+      set({ isSigningUp: true });
+      try {
+        const res = await axiosInstance.post("/auth/signup", data);
+        set({ authUser: res.data });
+        toast.success("Account created successfully");
+        // get().connectSocket();
+      } catch (error) {
+        toast.error(error.response.data.message);
+      } finally {
+        set({ isSigningUp: false });
+      }
+    },
 
+    login: async (data) => {
+      set({ isLoggingIn: true });
+      try {
+        const res = await axiosInstance.post("/auth/login", data);
+        set({ authUser: res.data });
+        toast.success("Logged in successfully");
+  
+        // get().connectSocket();
+      } catch (error) {
+        toast.error(error.response.data.message);
+      } finally {
+        set({ isLoggingIn: false });
+      }
+    },
+  
+    logout: async () => {
+      try {
+        await axiosInstance.post("/auth/logout");
+        set({ authUser: null });
+        toast.success("Logged out successfully");
+        // get().disconnectSocket();
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    },
+
+    updateProfile: async (data) => {
+      set({ isUpdatingProfile: true });
+      try {
+        const res = await axiosInstance.put("/auth/update-profile", data);
+        set({ authUser: res.data });
+        toast.success("Profile updated successfully");
+      } catch (error) {
+        console.log("error in update profile:", error);
+        toast.error(error.response.data.message);
+      } finally {
+        set({ isUpdatingProfile: false });
+      }
     },
 }))
