f20a15391f4bd9d19be57207a45e1d681d21b8f6
Harshit <sainiharshit322@gmail.com>
2024-12-24 23:35:30 +0530

    added profile page functionality

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
     authUser: null,
